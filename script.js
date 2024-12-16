9616c80d52d61d14b9675ab49e0ce8393e1870d3
ayush9024 <ayushnayak9024@gmail.com>
2024-12-17 00:42:21 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('button[type="submit"]').addEventListener('click', function() {
+    alert('Booking Done! Our Team Will connect you soon');
+});
+document.querySelector('button[type="button"]').addEventListener('click', function() {
+    alert('Payment Succesfully Done');
+});
